e479ceedc12576c7c585c673f7a869ef122d58d5
Marine <marine.lemetayer.pro@gmail.com>
2018-04-05 16:03:45 +0200

    Setup for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000; //Setting up for Heroku PORT
+
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -57,6 +59,6 @@ app.get('/bad', (req, res) => {
 });
 
 /** Start server **/
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 } );
