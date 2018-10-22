076abe96c19d174fd043c5c423cc25d26f42dcfb
Marine <marine.lemetayer.pro@gmail.com>
2018-10-22 15:20:40 +0200

    Test 404

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use( (req, res, next) => {
     let now = new Date().toString();
     let log = `${now} : ${req.method} ${req.url}`;
 
+    //Log content
     fs.appendFile('server.log', log + '\n', (err) => {
         if( err ) console.log('Unable to append to server.log');
     } );
@@ -27,6 +28,7 @@ app.use( (req, res, next) => {
 //     });
 // });
 
+//Load static files
 app.use( express.static(__dirname + '/public') );
 
 /** Helpers **/
